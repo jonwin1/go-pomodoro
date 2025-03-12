43d88a5a6137087a3bb8c35bf00f75bea7424123
jonwin1 <jonatan.wincent@gmail.com>
2025-03-12 21:26:43 +0100

    mwe

--- pomodoro.go
+++ pomodoro.go
@@ -3,21 +3,55 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+	"time"
 )
 
 func main() {
 	// Define flags with default values and descriptions
 	workLen := flag.Int("w", 25, "Work length in minutes")
 	breakLen := flag.Int("b", 5, "Break length in minutes")
-	longBreakLen := flag.Int("l", 15, "Long break length in minutes")
-	sessions := flag.Int("s", 4, "Number of sessions")
+	// longBreakLen := flag.Int("l", 15, "Long break length in minutes")
+	// sessions := flag.Int("s", 4, "Number of sessions")
 
 	// Parse the command-line arguments
 	flag.Parse()
 
 	// Print the values
-	fmt.Println("work len:", *workLen)
-	fmt.Println("break len:", *breakLen)
-	fmt.Println("long break len:", *longBreakLen)
-	fmt.Println("sessions:", *sessions)
+	workMsg := "Work for " + strconv.Itoa(*workLen) + " minutes"
+	breakMsg := "Break for " + strconv.Itoa(*breakLen) + " minutes"
+
+	fmt.Println("work len:", *workLen, "m")
+	fmt.Println("break len:", *breakLen, "m")
+	// fmt.Println("long break len:", *longBreakLen, "m")
+	// fmt.Println("sessions:", *sessions, "m")
+
+	for {
+		cmd := exec.Command("notify-send", "Pomodoro", workMsg)
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(time.Duration(*workLen) * time.Minute)
+
+		cmd = exec.Command("notify-send", "Pomodoro", "Session ended")
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+		cmd = exec.Command("notify-send", "Pomodoro", "Work session ended\n" + breakMsg)
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(time.Duration(*breakLen) * time.Minute)
+
+		cmd = exec.Command("notify-send", "Pomodoro", "Session ended")
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
