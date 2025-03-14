6dc107a9d227fd1c4cb44d0acf1cbbaa3efd8d17
jonwin1 <jonatan.wincent@gmail.com>
2025-03-14 23:50:46 +0100

    notification sound, clean exit, start/stop icon, ...

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # Go-Pomodoro
 
-Currenty a work in progress
+A simple pomodoro program intended for use with Waybar.
+
+## Installation
+
+### Prerequisite
+
+- pkg-config
+- alsa-lib
+
+### Pomodoro
+
+Clone this repository, navigate into it, and run `go build`.
+
+### Waybar
+
+Put this in waybar config and add `custom/pomodoro` to one of the modules.
+Remember to replace <path-to-go-pomodoro> with the path to your clone of this repo.
+
+```
+"custom/pomodoro" = {
+    format = "{}";
+    signal = 10;
+    return-type = "json";
+    exec = "cat <path-to-go-pomodoro>/log";
+    on-click = "bash -c 'pgrep pomodoro && pkill pomodoro || <path-to-go-pomodoro>/pomodoro &'";
+};
+
+## TODO
+
+- [x] Notifications
+- [x] Waybar integration
+- [x] Notification sound
+- [ ] Better output file location
+- [ ] Proper install
+- [ ] Flake.nix
+- [ ] Documentation
+- [ ] Longer break everly few sessions
--- pomodoro.go
+++ pomodoro.go
@@ -1,52 +1,113 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
+
+	"github.com/go-audio/wav"
+	"github.com/hajimehoshi/oto"
 )
 
-func main() {
-	// Define flags with default values and descriptions
-	workLen := flag.Int("w", 25, "Work length in minutes")
-	breakLen := flag.Int("b", 5, "Break length in minutes")
-	// longBreakLen := flag.Int("l", 15, "Long break length in minutes")
-	// sessions := flag.Int("s", 4, "Number of sessions")
+// Uppdates the text in the bar by writing to file
+// and signaling waybar to read the change
+func updateBar(str string) {
+	// Create/truncate file
+	file, err := os.Create("/home/jonwin/go-pomodoro/log")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close() // Close when done
 
-	// Parse the command-line arguments
-	flag.Parse()
+	// Write meassage to file
+	_, err = file.WriteString(str)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
 
-	// Print the values
-	workMsg := "Work for " + strconv.Itoa(*workLen) + " minutes"
-	breakMsg := "Break for " + strconv.Itoa(*breakLen) + " minutes"
-
-	fmt.Println("work len:", *workLen, "m")
-	fmt.Println("break len:", *breakLen, "m")
-	// fmt.Println("long break len:", *longBreakLen, "m")
-	// fmt.Println("sessions:", *sessions, "m")
-
-	cmd := exec.Command("notify-send", "Pomodoro", workMsg)
+	// Signal waybar to update
+	cmd := exec.Command("pkill", "-SIGRTMIN+10", "waybar")
 	if err := cmd.Run(); err != nil {
+		log.Fatal("Error sending signal to waybar:", err)
+	}
+}
+
+// Send a notification with a message
+func notifySend(msg string) {
+	data, err := os.ReadFile("./mixkit-correct-answer-tone-2870.wav")
+	if err != nil {
+		panic("reading my-file.mp3 failed: " + err.Error())
+	}
+
+	dec := wav.NewDecoder(bytes.NewReader(data))
+	buf, err := dec.FullPCMBuffer()
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	for {
-		time.Sleep(time.Duration(*workLen) * time.Minute)
-
-		cmd = exec.Command("notify-send", "Pomodoro", "Work session ended\n"+breakMsg)
-		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
-
-		time.Sleep(time.Duration(*breakLen) * time.Minute)
-
-		cmd := exec.Command("notify-send", "Pomodoro", "Break is over\n"+workMsg)
-		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
+	byteBuffer := make([]byte, len(buf.Data)*2)
+	for i, sample := range buf.Data {
+		binary.LittleEndian.PutUint16(byteBuffer[i*2:], uint16(sample))
 	}
 
+	ctx, err := oto.NewContext(buf.Format.SampleRate, buf.Format.NumChannels, 2, 4096)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	player := ctx.NewPlayer()
+	_, err = player.Write(byteBuffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	player.Close()
+	ctx.Close()
+
+	cmd := exec.Command("notify-send", "Pomodoro", msg)
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Start a session with length l and type t
+func session(l int, t string) {
+	for i := range l {
+		updateBar("{\"text\": \"󰓛 " + t + " " + strconv.Itoa(l-i) + " min\"}")
+		time.Sleep(time.Minute)
+	}
+}
+
+func main() {
+	// Parse flags
+	workLen := flag.Int("w", 25, "Work length in minutes")
+	breakLen := flag.Int("b", 5, "Break length in minutes")
+	flag.Parse()
+
+	// Capture interupt/terminate signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// On received signal, update bar to reflect stop and exit
+	go func() {
+		sig := <-sigs
+		fmt.Println("Received signal:", sig)
+		updateBar("{\"text\": \"󰐊 Work " + strconv.Itoa(*workLen) + " min\"}")
+		os.Exit(0)
+	}()
+
+	for {
+		session(*workLen, "Work")
+		notifySend("Time for a short break.")
+		session(*breakLen, "Break")
+		notifySend("Let's get back to work.")
+	}
 }
