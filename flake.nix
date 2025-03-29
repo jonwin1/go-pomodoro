e49d4288d5cfdb9c6532c80b3f6bf1d887fd3eea
jonwin1 <jonatan.wincent@gmail.com>
2025-03-29 19:53:21 +0100

    update flake, add envrc

--- flake.nix
+++ flake.nix
@@ -1,41 +1,65 @@
 {
-    description = "A simple pomodoro program intended for use with Waybar";
+  description = "A simple pomodoro program intended for use with Waybar";
 
-    inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
 
-    outputs = { self, nixpkgs }: let
-        forAllSystems = function:
-            nixpkgs.lib.genAttrs [
-                "x86_64-linux"
-                "x86_64-darwin"
-                "aarch64-linux"
-                "aarch64-darwin"
-            ] (system: function nixpkgs.legacyPackages.${system});
-    in {
-        packages = forAllSystems (pkgs: {
-            default = pkgs.buildGoModule {
-                pname = "pomodoro";
-                version = self.shortRev or self.dirtyShortRev;
-                src = ./.;
-                nativeBuildInputs = with pkgs; [ pkg-config ];
-                buildInputs = with pkgs; [ alsa-lib ];
-                # vendorHash = pkgs.lib.fakeHash;
-                vendorHash = "sha256-z/ZM97Ti9hYF+B8EbwvqXKlgzzd2MBBeiNtA9wH1jwU=";
-            };
-        });
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
 
-        devShells = forAllSystems (pkgs: {
-            default = pkgs.mkShell {
-                buildInputs = with pkgs; [
-                    go
-                    gopls
-                    go-tools
-                    gotools
+      commonDeps = pkgs: {
+        # Run-time dependencies
+        buildInputs = with pkgs; [
+          alsa-lib
+        ];
 
-                    pkg-config
-                    alsa-lib
-                ];
-            };
-        });
+        # Build-time dependencies
+        nativeBuildInputs = with pkgs; [
+          go
+          gopls
+          pkg-config
+        ];
+      };
+    in
+    {
+      packages = forAllSystems (
+        pkgs:
+        let
+          deps = commonDeps pkgs;
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "pomodoro";
+            version = self.shortRev or self.dirtyShortRev;
+            src = ./.;
+
+            buildInputs = deps.buildInputs;
+            nativeBuildInputs = deps.nativeBuildInputs;
+
+            vendorHash = "sha256-z/ZM97Ti9hYF+B8EbwvqXKlgzzd2MBBeiNtA9wH1jwU=";
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        pkgs:
+        let
+          deps = commonDeps pkgs;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = deps.buildInputs;
+            nativeBuildInputs = deps.nativeBuildInputs;
+          };
+        }
+      );
     };
 }
